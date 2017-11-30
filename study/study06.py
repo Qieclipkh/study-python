14282f22177da822423674c944e62117939235fc
Qieclipkh <cly19892007@163.com>
2017-11-30 23:43:22 +0800

    学习了作用域，globals()函数 locals()函数 global关键字 函数内修改全局变量的值 nonlocal关键字 内层函数修改外层函数的值 闭包（在函数里边定义一个函数）

--- study/study06.py
+++ study/study06.py
@@ -93,3 +93,43 @@ print((5,)*2)
 
 
 #作用域121
+#变量和对应的值用的是个“不可见”字典，內建的var函数可以返回这个字典
+# 一般来说vars返回的字典是不能修改的，官方文档说，结果是未定义的，可能得不到想要的结果
+x=9000
+scope = vars()
+print(scope)
+scope['x']
+print(scope['x'])
+scope['x']+=1000
+print(x)
+
+#函数内的变量被称为局部变量，与全局变量想法的概念
+#如果局部变量名和全局变量名相同，全局变量会被局部变量屏蔽，如果要获取全局变量使用globals函数
+x=1000000
+def xx(x):
+    print(x)
+    print(globals()['x'])
+xx(1)
+print('+++++')
+#重绑定全局变量（使变量引用新值）
+def change_global():
+    global x
+    x = x+1
+change_global()
+
+print(x)
+#闭包，函数里边放一个函数
+def foo(x):
+    def doo(y):
+        #nonlocal关键字和global关键字类似，可以让内层函数对外层函数的变量进行复赋值
+        nonlocal x
+        x=x+4
+        print(x*y)
+    return doo
+#将内层函数作为外层函数的返回值，但使没有被调用
+x = foo(3)
+print(x)
+x(5)
+foo(3)(5)
+
+
